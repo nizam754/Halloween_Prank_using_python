3e7139bfd5c8ca707763bad57bd4abeaabb87b40
nizam754 <m.nizam754@outlook.com>
2021-02-17 17:21:57 +0600

    bug fixed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
 from time import sleep
 pygame.init()
-window = pygame.display.set_mode((0,0, pygame.FULLSCREEN))
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 sleep(3)
